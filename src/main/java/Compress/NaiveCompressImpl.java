4d8960dd17d63393252705f894b316855e0f788c
Michal <michalr11@gmail.com>
2019-03-30 14:18:47 +0100

    Lambdy

--- src/main/java/Compress/NaiveCompressImpl.java
+++ src/main/java/Compress/NaiveCompressImpl.java
@@ -5,40 +5,22 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class NaiveCompressImpl implements Compressor {
+
+
     @Override
     public String compress(String input) {
-        if (input == null || input.isEmpty())
-            return "";
 
-        Arrays.stream(input.split(" ")).map(
-                word -> word.substring(0,1).toUpperCase()+
-                        word.substring(1).toLowerCase())
-                .collect(Collectors.joining(""));
-
-
-        String[] words = input.split(" ");
-        StringBuilder sb = new StringBuilder();
-        for (String word : words) {
-            sb.append(word.substring(0, 1).toUpperCase());
-            sb.append(word.substring(1).toLowerCase());
-
-        }
-
-
-        return sb.toString();
+        return Arrays.stream(input.split(" ")).map(
+                word -> word.substring(0, 1).toUpperCase()
+                        + word.substring(1).toLowerCase()).collect(Collectors.joining(""));
     }
 
     @Override
     public String uncompress(String input) {
-        if (input == null || input.isEmpty())
-            return "";
-        String[] words = input.split("(?=\\p{Lu})");
-        StringBuilder sb = new StringBuilder();
-        for (String word : words) {
-            sb.append(word.toLowerCase());
-            sb.append(" ");
-        }
-        return sb.toString().trim();    // trim usuwa spację
-    }
+        return Arrays.stream(input.split(" (?=\\p{Lu})"))
+                .map(String::toLowerCase)
+                .collect(Collectors.joining(" "));
 
+    }
 }
+
